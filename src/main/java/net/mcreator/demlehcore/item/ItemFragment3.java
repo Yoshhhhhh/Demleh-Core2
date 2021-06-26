6ec8d0dc2b7e8f485a0ae76f3756c59226814b53
Yoshhhhhh <super@DESKTOP-9EEQ732>
2021-06-26 19:06:05 +0200

    update faux et fragments

---
+++ src/main/java/net/mcreator/demlehcore/item/ItemFragment3.java
@@ -0,0 +1,66 @@
+
+package net.mcreator.demlehcore.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.demlehcore.ElementsDemlehcoreMod;
+
+@ElementsDemlehcoreMod.ModElement.Tag
+public class ItemFragment3 extends ElementsDemlehcoreMod.ModElement {
+	@GameRegistry.ObjectHolder("demlehcore:fragment_3")
+	public static final Item block = null;
+	public ItemFragment3(ElementsDemlehcoreMod instance) {
+		super(instance, 228);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("demlehcore:fragment_3", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("fragment_3");
+			setRegistryName("fragment_3");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
